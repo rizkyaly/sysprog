2768344e43aa9f65488c08cc9afca565b3d36989
rizkyaly <rizkyaly@gmail.com>
2017-10-11 01:51:47 +0700

    hw6 updated

--- sysinfo.sh
+++ sysinfo.sh
@@ -18,23 +18,32 @@ flag=1
 if [ $flag -eq 1 ];then
 	if [ $input -eq 1 ]; then
 		echo "Username : "$USER
-		cat /proc/version | awk '{print "OS : "$1" "$2" "$3}'
+		cat /proc/version | awk '{print "OS : "$1,$2,$3}'
+		uptime | sed -e 's/  / /g' | grep '[0-9]:[0-9][0-9], [0-9]' -o | awk '{print "Uptime : "$0}'
+		ifconfig | grep 'inet addr:192.[0-9.//]*' -o | grep '[0-9.//]*' -o | awk '{print "IP : "$1}'
+		cat /etc/hostname | awk '{print "Hostname : "$1}'
 		printf "Press [Enter] key to continue..."
 		read enter
 		flag=0
 	elif [ $input -eq 2 ];then
-		echo "2. Hardware List"
+		lscpu | sed -e 's/Architecture:/ /' | grep 'x.*' -o | awk 'NR==1{print "Machine Hardware : "$1}'
+		sudo lshw -short
 		printf "Press [Enter] key to continue..."
 		read enter
 		flag=0
 	elif [ $input -eq 3 ];then
-		echo "3. Free and Used Memory"
+		free -m | awk 'NR==2{print "Size : "$2" MB"}'
+		echo "Memory Statistics"
+		vmstat
+		echo "Top 10 cpu eating process"
+		ps aux --sort=-%mem | head -n 10
 		printf "Press [Enter] key to continue..."
 		read enter
 		flag=0
 	elif [ $input -eq 4 ];then
 		echo "4. Hardware Detail"
 		printf "Press [Enter] key to continue..."
+		read enter
 		flag=0
 	elif [ $input -eq 5 ];then
 		exit;
