7a8bd96aacee91b11f79694ce555f5a4cb2425bb
rizkyaly <rizkyaly@gmail.com>
2017-10-11 23:09:49 +0700

    done cuyz:

--- sysinfo.sh
+++ sysinfo.sh
@@ -17,6 +17,9 @@ read input
 flag=1
 if [ $flag -eq 1 ];then
 	if [ $input -eq 1 ]; then
+		echo "--------------------------------------------------"
+		echo "     System Status"
+		echo "--------------------------------------------------"
 		echo "Username : "$USER
 		cat /proc/version | awk '{print "OS : "$1,$2,$3}'
 		uptime | sed -e 's/  / /g' | grep '[0-9]:[0-9][0-9], [0-9]' -o | awk '{print "Uptime : "$0}'
@@ -32,20 +35,57 @@ if [ $flag -eq 1 ];then
 		read enter
 		flag=0
 	elif [ $input -eq 3 ];then
+		echo "--------------------------------------------------"
+		echo "     MEMORY"
+		echo "--------------------------------------------------"
+		echo "****************"
+		echo "     Memory"
+		echo "****************"
 		free -m | awk 'NR==2{print "Size : "$2" MB"}'
-		echo "Memory Statistics"
+		free -m | awk 'NR==2{print "Free : "$4" MB"}'
+		echo "******************************"
+		echo "     Memory Statistics"
+		echo "******************************"
 		vmstat
-		echo "Top 10 cpu eating process"
+		echo "*************************************"
+		echo "     Top 10 cpu eating process"
+		echo "*************************************"
 		ps aux --sort=-%mem | head -n 10
 		printf "Press [Enter] key to continue..."
 		read enter
 		flag=0
 	elif [ $input -eq 4 ];then
-		echo "4. Hardware Detail"
-		printf "Press [Enter] key to continue..."
-		read enter
-		flag=0
+		while [ $flag -eq 1 ]; do
+		echo "========================="
+		echo "     Hardware Detail"
+		echo "========================="
+		echo "1. CPU"
+		echo "2. Block Devices"
+		echo "3. Back"
+		printf "Choose 1-3 : "
+		read input2
+			if [ $input2 -eq 1 ];then
+			echo "--------------------------------------------------"
+			echo "     CPU"
+			echo "--------------------------------------------------"
+			cat /proc/cpuinfo | grep 'model name' | cut -f 2- | awk 'NR==1{print "Model Name "$0}'
+			cat /proc/cpuinfo | grep 'cpu MHz' | cut -f 3- | awk 'NR==1{print "Frequency "$0}'
+			cat /proc/cpuinfo | grep 'cache size' | cut -f 2- | awk 'NR==1{print "Cache "$0}'
+			printf "Press [Enter] key to continue..."
+			read enter
+			elif [ $input2 -eq 2 ];then
+			echo "--------------------------------------------------"
+			echo "     blk"
+			echo "--------------------------------------------------"
+			lsblk
+			printf "Press [Enter] key to continue..."
+			read enter
+			elif [ $input2 -eq 3 ];then
+			flag=0
+			fi
+		done
 	elif [ $input -eq 5 ];then
+		echo "Bye Bye...."
 		exit;
 	fi
 fi
